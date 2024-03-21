d74451a6f446e5dfa3b73b8f55219063ac2c7f15
singuluri sudha <sudhasinguluri@gmail.com>
2024-03-21 17:29:52 +0530

    updated_code

--- restaurent_project/urls.py
+++ restaurent_project/urls.py
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-"""
-URL configuration for restaurent_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-
-from django.urls import path , include
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/restaurent/',include('restaurent.api_urls')),
-
-]
-
-
-=======
 """
 URL configuration for restaurent_project project.
 
@@ -57,4 +27,3 @@ urlpatterns = [
 
 # if settings.DEBUG:
 #     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
->>>>>>> 4e55e992b6d010b449c0fed1cb2e78b58e7b8c67
