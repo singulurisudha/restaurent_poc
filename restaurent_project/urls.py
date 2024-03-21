65389d47c8f41e53d7069cc0b3ba2cabdf02d95a
singuluri sudha <sudhasinguluri@gmail.com>
2024-03-21 17:21:13 +0530

    updated-code

--- restaurent_project/urls.py
+++ restaurent_project/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from restaurent_project import settings
+
 from django.urls import path , include
-from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,5 +25,4 @@ urlpatterns = [
 
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
