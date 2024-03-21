a791d1c97b204e2bb30c3e5dbe6c1bee75052c65
S Sudha <89729299+singulurisudha@users.noreply.github.com>
2024-03-21 17:08:39 +0530

    Update settings.py

--- restaurent_project/settings.py
+++ restaurent_project/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
